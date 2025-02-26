fb7d8d2ce8a382d8e66d753947113f30e7b39d45
RenzoWille <renzo.wille@hva.nl>
2025-02-26 11:08:06 +0100

    #12 hamburger menu met css en js eerste opzet

---
+++ public/main.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.getElementById('hamburger-button');
+    const menu = document.getElementById('menu');
+
+    // Open of sluit het menu wanneer op het icoon wordt geklikt
+    hamburger.addEventListener('click', (event) => {
+        menu.classList.toggle('open');
+        event.stopPropagation(); // Voorkomt dat de klik ook het document-event triggert
+    });
+
+    // Sluit het menu wanneer ergens buiten het menu wordt geklikt
+    document.addEventListener('click', (event) => {
+        if (menu.classList.contains('open')) {
+            menu.classList.remove('open');
+        }
+    });
+});
