ef444c112528484db82b54c0b8b76f5f13eb686e
Safae-e <Safae.el.jaouhari@hva.nl>
2025-02-26 11:24:07 +0100

    update index.liquid

--- public/main.js
+++ public/main.js
@@ -1,9 +1,2 @@
-document.getElementById("like").addEventListener("click", function(){
-    // Selecteer het like count element
-    const likeCount = document.getElementById("like-count");
-    
-    // huidige tekst vervangen met optelsom 
-    // parseInt = zet text om naar cijfers
-    likeCount.textContent = parseInt(likeCount.textContent) +1;
-})
+
 
