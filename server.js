39d4a57d8d10efd40e592c7dc398666b247258cf
Sidopjescherm <hermussidney@gmail.com>
2025-02-28 01:22:25 +0100

    #24
    hier verwijder ik code die ik niet meer nodig heb

--- server.js
+++ server.js
@@ -22,17 +22,15 @@ app.get('/', async function (request, response) {
   const messagesResponse = await fetch(`https://fdnd.directus.app/items/messages/?filter={"for":"Team ${teamName}"}`)
   const messagesResponseJSON = await messagesResponse.json()
 
-  const squadResponse = await fetch('https://fdnd.directus.app/items/squad?filter={"_and":[{"cohort":"2425"},{"tribe":{"name":"FDND Jaar 1"}}]}')
-  const squadResponseJSON = await squadResponse.json()
   // const squadResponse = await fetch('https://fdnd.directus.app/items/squad?filter={"_and":[{"cohort":"2425"},{"tribe":{"name":"FDND Jaar 1"}}]}')
   // const squadResponseJSON = await squadResponse.json()
+
   const personResponse = await fetch('https://fdnd.directus.app/items/person/?fields=*,squads.squad_id.name,squads.squad_id.cohort&filter={"_and":[{"squads":{"squad_id":{"tribe":{"name":"FDND Jaar 1"}}}},{"squads":{"squad_id":{"cohort":"2425"}}}]}')
   const personResponseJSON = await personResponse.json()
 
   response.render('index.liquid', {
     teamName: teamName,
     messages: messagesResponseJSON.data,
-    squads: squadResponseJSON.data
     // squads: squadResponseJSON.data
   })
 })
@@ -102,29 +100,3 @@ app.set('port', process.env.PORT || 8000)
   app.listen(app.get('port'), function () {
     console.log(`Application started on http://localhost:${app.get('port')}`)
   })
-
-  // De links naar alle teampagina's gaan hier staan
-  app.get('/teams/:team', async function (request, response) {
-    console.log(request.params.team)
-  
-    const teamResponse = await fetch ('https://fdnd.directus.app/items/person/?filter={"team":{"_contains":"' + request.params.team + '"}}')
-    const teamResponseJSON = await teamResponse.json()
-  
-    console.log(teamResponseJSON)
-  
-    // Als de data 'leeg', laat dan een andere pagina zien...
-    if (teamResponseJSON.data.length == 0) {
-  
-    } else 
-      response.render('teamleden.liquid', {persons: teamResponseJSON.data, squads: squadResponseJSON.data, team_name: request.params.team})
-  })
-  
-  
-
-//   app.get('/detail:id', async function (request, response) {
-//     const personDetailResponse = await fetch('https://fdnd.directus.app/items/person/' + request.params.id)
-//     const personDetailResponseJSON = await personDetailResponse.json()
-//     const squadResponseJSON = await squadResponseJSON.json()
-
-//   response.render('detail_student.liquid', {person: personDetailResponseJSON.data, squads: squadResponseJSON.data})
-// })
